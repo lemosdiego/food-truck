25f28bf9030a8e66ff78f4e37f5347fd77f4d32f
Washington <washington.contato1327@gmail.com>
2025-06-12 09:50:37 -0300

    style(global): ajusta estilos globais e layout da página de burguer

--- src/app/burguer/page.js
+++ src/app/burguer/page.js
@@ -4,7 +4,6 @@ import Contacts from "@/sections/contacts/Contacts";
 import Differential from "@/sections/differential/Differential";
 import Footer from "@/sections/footer/Footer";
 import Hero from "@/sections/hero/Hero";
-import Schedule from "@/sections/schedule/Schedule";
 import Testimonials from "@/sections/testimonials/Testimonials";
 
 export default function BurguerPage() {
